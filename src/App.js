965099c0c5596e9ec6f8108903a0384e481aca78
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-23 14:58:44 +0545

    github users fetched into users component

--- src/App.js
+++ src/App.js
@@ -6,22 +6,30 @@ import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    users: [],
+    loading: false
+  }
+
   async componentDidMount() {
+    this.setState({loading: true});
+
     const res = await axios.get('https://api.github.com/users');
 
-    console.log(res.data);
+    //console.log(res.data);
+
+    this.setState({users: res.data, loading: false});
   }
 
   render () {
     return (
       <div className="App">
         <Navbar title="Github Profile Finder" icon="fab fa-github" />
-        <div className="container"><Users /></div>
+        <div className="container"><Users loading={this.state.loading} users={this.state.users}/></div>
       </div>
     );
   }
-
-  
+ 
 }
 
 export default App;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,35 +3,35 @@ import UserItem from './UserItem';
 
 class Users extends Component {
 
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'evanphx',
-                avatar_url: 'https://avatars.githubusercontent.com/u/7?v=4',
-                html_url: 'https://github.com/evanphx'
-            },
+    // state = {
+    //     users: [
+    //         {
+    //             id: '1',
+    //             login: 'evanphx',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/7?v=4',
+    //             html_url: 'https://github.com/evanphx'
+    //         },
 
-            {
-                id: '2',
-                login: 'vanpelt',
-                avatar_url: 'https://avatars.githubusercontent.com/u/17?v=4',
-                html_url: 'https://github.com/vanpelt'
-            },
+    //         {
+    //             id: '2',
+    //             login: 'vanpelt',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/17?v=4',
+    //             html_url: 'https://github.com/vanpelt'
+    //         },
             
-            {
-                id: '3',
-                login: 'pjhyett',
-                avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-                html_url: 'https://github.com/pjhyett'
-            }
-        ]
-    }
+    //         {
+    //             id: '3',
+    //             login: 'pjhyett',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
+    //             html_url: 'https://github.com/pjhyett'
+    //         }
+    //     ]
+    // }
 
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
