12f72926c35b0c3455a32493098ad02814ff3ae4
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-23 14:53:24 +0545

    axios implementation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
 
-  componentDidMount() {
-    
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+
+    console.log(res.data);
   }
 
   render () {
