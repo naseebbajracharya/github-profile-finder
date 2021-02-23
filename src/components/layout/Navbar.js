de1f626db0025a4657288e2afcbbc9630c75b93b
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-23 14:03:46 +0545

    started with user item component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Navbar extends Component {
+
+    static defaultProps = {
+        title: 'Github Profile Finder',
+        icon: 'fab fa-github'
+    }
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    }
+
+    render() {
+        return (
+            <nav className="navbar bg-dark">
+                <h1>
+                <i class={this.props.icon}></i> {this.props.title}
+                </h1>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
