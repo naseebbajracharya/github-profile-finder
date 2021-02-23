574f9d802285ec28aeb0552a387e39e4babe5b5c
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-23 14:18:55 +0545

    started with user item component 2

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class UserItem extends Component {
+
+    state = {
+        id: 'id',
+        login: 'evanphx',
+        avatar_url: 'https://avatars.githubusercontent.com/u/7?v=4',
+        html_url: 'https://github.com/evanphx'
+
+    }
+
     render() {
+
+        const {login,avatar_url,html_url} = this.state;
+
         return (
-            <div>
-                UserItem
+            <div className="card text-center">
+                <img src={avatar_url} alt="img" className="round-img" style={{width: '80px'}} />
+                <h3>{this.state.login}</h3>
+                <a href={this.state.html_url} className="btn btn-primary2 btn-sm my-1">More</a>
             </div>
         )
     }
 }
 
-export default UserItem
+export default UserItem;
