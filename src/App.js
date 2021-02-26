3299b7f73c0cb207cc8ca689f14de551d57e56e6
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-26 17:19:27 +0545

    spinner line added; search fnc

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ class App extends Component {
 
   //search for github users
   searchUsers = async (text) => {
+
+    this.setState({loading: true});
+    
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     //console.log(res.data);
