238fb4d59c0d67caa6a90eb1eda3e066d33910f3
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-03-06 22:51:17 +0545

    works on users repos

--- src/App.js
+++ src/App.js
@@ -51,6 +51,17 @@ class App extends Component {
     this.setState({user: res.data, loading: false});
   }
 
+  //get users repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+    
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    //console.log(res.data);
+
+    this.setState({repos: res.data, loading: false});
+  }
+
   //clear user from state
   clearUsers = () => {
     this.setState({users: [], loading:false});
