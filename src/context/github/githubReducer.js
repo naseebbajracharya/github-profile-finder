12d12dc13851ba79731674e4058a31e443611806
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-03-18 18:49:08 +0545

    adding up cases in githubreducer

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -5,3 +5,23 @@ import {
     GET_REPOS,
     GET_USER
 } from '../types';
+
+export default (state, action) => {
+    switch(action.type) {
+        case SEARCH_USERS:
+            return {
+                ...state,
+                users: action.payload,
+                loading: false 
+            }
+
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+
+        default:
+            return state;
+    }
+}
