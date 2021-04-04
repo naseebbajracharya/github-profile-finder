c8140442bed54dfaaa4c5cff0108ab7aeab31c9c
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-04-04 20:20:55 +0545

    handling proptype errors

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -75,10 +75,7 @@ const User = ({ getUserRepos, repos, match}) => {
 }
 
 User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
     repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired,
 }
 
