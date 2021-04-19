ae2707b26500539a8b77807a07ac15ed369427b3
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-04-19 19:58:24 +0545

    alert context

---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import AlertContext from './alertContext';
+import alertReducer from './alertReducer';
+import {
+    SET_ALERT, REMOVE_ALERT
+} from '../types';
+
+const AlertState = props => {
+    const initialState = null;
+
+    const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+    
+    //SET ALERT
+
+
+return <AlertContext.Provider value={{alert: state.users, user: state.user}}>{props.children}</AlertContext.Provider>
+}
+
+export default AlertState;
